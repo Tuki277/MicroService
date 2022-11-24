7ae809a35595ce81f96fcb963b4629f67a4a627c
tuki <huybeos2707@gmail.com>
2022-11-24 09:04:06 +0700

    update db file

--- Shop.sql
+++ Shop.sql
@@ -49,7 +49,7 @@ INSERT INTO `Category` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `Code` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `code` varchar(255) NOT NULL,
   `discount` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
